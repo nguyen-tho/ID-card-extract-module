3f5e6033805316849d182816ed77fb55d1a23d16
nguyen-tho <69784527+nguyen-tho@users.noreply.github.com>
2023-11-29 14:42:39 +0700

    update load image feature

--- app.py
+++ app.py
@@ -1,29 +1,41 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 from process import processing  # Import the processing function from your module
 import sys
 import postprocessing as post
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
 app.json.ensure_ascii = False
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-@app.route('/', methods=['GET'])
-def run_processing():
+@app.route('/', methods=['POST', 'GET'])
+def upload():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        image = request.files['image']
+        return redirect(f'/process/<{username}>/<{image.filename}>'), 200
+    else:
+        return render_template('index.html')
+    
+@app.route('/process/<username>/<image>', methods=['GET'])
+def run_processing(username, image):
+    file_path = os.path.join('uploads', image)
     try:
         # Assuming the input is a JSON payload with file_path and output_name
-        
-        
-        # Extract parameters from the JSON payload
-        file_path = request.args.get('param1', default=sys.argv[1] if len(sys.argv) > 1 else None)
-        output_name = request.args.get('param2', default=sys.argv[2] if len(sys.argv) > 2 else None)
 
+        # Extract parameters from the JSON payload
+        # file_path = request.args.get('param1', default=sys.argv[1] if len(sys.argv) > 1 else None)
+        # output_name = request.args.get('param2', default=sys.argv[2] if len(sys.argv) > 2 else None)
 
         # Check if the required parameters are present
-        if not file_path or not output_name:
+        if not file_path or not image:
             return jsonify({'error': 'Missing required parameters'}), 400
 
         # Call the processing function with the provided parameters
-        processing(file_path, output_name)
-        result = post.read_json_file(f'output/{output_name}/result.json')
+        processing(file_path, username)
+        result = post.read_json_file(f'output/{username}/result.json')
 
         # Return the result as JSON
         return jsonify(result)
@@ -31,5 +43,8 @@ def run_processing():
         # Handle any errors that may occur during the processing
         return jsonify({'error': str(e)}), 500
 
+
 if __name__ == '__main__':
+    if not os.path.exists('uploads'):
+        os.makedirs('uploads')
     app.run(debug=True)
